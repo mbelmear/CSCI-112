5e0f8112a667f7b072cded68ffde0be6092a5769
Michael Belmear <90008055+mbelmear@users.noreply.github.com>
2022-09-09 16:42:36 -0600

    Add files via upload

---
+++ Classwork1/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*/first classwork
+//Michael Belmear
+//8/29/22*/
+int main(){
+    //printing "Hello World"
+    printf("Hello World\n");
+    //exit with no errors
+    return(0);
+}
